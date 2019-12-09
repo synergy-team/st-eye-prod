a615720c1a6405e91b0c0e02768f20478790213c
Peter <p.borisenko@synergy.msk.ru>
2019-12-09 17:00:35 +0300

    v3.0

---
+++ precache-manifest.2a6d2914a9687ade333cf3ad194ea332.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd809bbbc90bb0a9f9d9fa650fbad25d",
+    "url": "/st-eye-prod/index.html"
+  },
+  {
+    "revision": "929a1bf4858bc5bb03dd",
+    "url": "/st-eye-prod/static/css/main.8fa579d1.chunk.css"
+  },
+  {
+    "revision": "990dbd296050a15a6fc9",
+    "url": "/st-eye-prod/static/js/2.d1083f4c.chunk.js"
+  },
+  {
+    "revision": "929a1bf4858bc5bb03dd",
+    "url": "/st-eye-prod/static/js/main.1ee0277c.chunk.js"
+  },
+  {
+    "revision": "8665c6033d675dc4e09e",
+    "url": "/st-eye-prod/static/js/runtime-main.f857497b.js"
+  },
+  {
+    "revision": "a203d5f499c0fa1dfe0fd2858b5718cc",
+    "url": "/st-eye-prod/static/media/analytics.a203d5f4.svg"
+  },
+  {
+    "revision": "7a07babbf823b3075861f489a4a83fa6",
+    "url": "/st-eye-prod/static/media/dispatching.7a07babb.svg"
+  }
+]);
